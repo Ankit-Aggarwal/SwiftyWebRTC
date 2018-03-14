b061a12af3e481e40ccd83abb0d34308f5e3ea44
Ankit Aggarwal <aren.ankit@gmail.com>
2018-03-14 11:55:18 +0530

    chore(swift4): move code to swift 4.0

--- SwiftyWebRTC/Code/AVCaptureState.swift
+++ SwiftyWebRTC/Code/AVCaptureState.swift
@@ -10,12 +10,12 @@ import AVFoundation
 
 class AVCaptureState {
     static var isVideoDisabled: Bool {
-        let status = AVCaptureDevice.authorizationStatus(forMediaType: AVMediaTypeVideo)
+        let status = AVCaptureDevice.authorizationStatus(for: AVMediaType.video)
         return status == .restricted || status == .denied
     }
 
     static var isAudioDisabled: Bool {
-        let status = AVCaptureDevice.authorizationStatus(forMediaType: AVMediaTypeAudio)
+        let status = AVCaptureDevice.authorizationStatus(for: AVMediaType.audio)
         return status == .restricted || status == .denied
     }
 }
