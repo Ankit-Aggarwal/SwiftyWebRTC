2aa160597648fc0f179a4f94501f2f833f3fe6d9
Ankit Aggarwal <aren.ankit@gmail.com>
2018-03-19 16:00:38 +0530

    feat(custom-capturer): update scope for various properties and methods

--- SwiftyWebRTC/Code/CustomCapturerer/RTCCapturer.swift
+++ SwiftyWebRTC/Code/CustomCapturerer/RTCCapturer.swift
@@ -8,7 +8,7 @@
 import Foundation
 import WebRTC
 
-class RTCCapturer: NSObject {
+public class RTCCapturer: NSObject {
 
     private var capturer: RTCCameraVideoCapturer
     private var settingsModel: RTCCapturerSettingsModel
