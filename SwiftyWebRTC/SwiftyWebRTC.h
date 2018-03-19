3c6b514537185e6f2382cfafadfad7ec66fb6658
Ankit Aggarwal <aren.ankit@gmail.com>
2018-03-19 15:39:07 +0530

    feat(custom-capturer): add custom capturer and related files

--- SwiftyWebRTC/SwiftyWebRTC.h
+++ SwiftyWebRTC/SwiftyWebRTC.h
@@ -16,3 +16,4 @@ FOUNDATION_EXPORT const unsigned char SwiftyWebRTCVersionString[];
 // In this header, you should import all the public headers of your framework using statements like #import <SwiftyWebRTC/PublicHeader.h>
 
 #import <WebRTC/WebRTC.h>
+
